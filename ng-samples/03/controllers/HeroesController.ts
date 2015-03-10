ef938abeebcee383747bcd6c772eb628c704ae4f
Jose I <jiparis@gmail.com>
2015-03-10 23:19:43 +0100

    Lección 3

---
+++ ng-samples/03/controllers/HeroesController.ts
@@ -0,0 +1,23 @@
+﻿module Controllers {
+
+    export class HeroesController {
+        static $inject = [
+            "$scope"
+        ];
+
+        heroes = ['Superman', 'The Green Steak', 'Scorpion Shadow', 'Doctor Cranium'];
+        showImg = false;
+
+        constructor(private $scope) {     
+            // setup scope       
+            $scope.heroes = this.heroes;
+            
+            // use arrow syntax for event handlers !!!
+            $scope.addName = (name: string) => {
+                this.heroes.push(name);
+                $scope.showImg = name == "Batman";
+            }          
+        }
+
+    }
+}
