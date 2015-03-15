1ad3e2b41777cb24b105beb4678a84019801ea2b
Jose I <jiparis@gmail.com>
2015-03-15 02:05:40 +0100

    filtros

---
+++ ng-samples/04/app.ts
@@ -0,0 +1,26 @@
+﻿/// <reference path="controllers/namesController.ts" />
+
+module Lesson04{
+    'use strict';
+
+    // nuestra aplicación angular. 
+    export var myApp: ng.IModule;
+
+    // clase de inicialización
+    class MyApp {
+        
+        /**
+        * Creación de la aplicación
+        */
+        static init() : ng.IModule {
+            return angular.module("myApp", [])
+                .controller("namesController", NamesController); // ["$scope", NamesController]);
+        }
+    };
+
+    export function init() {
+        myApp = MyApp.init();
+    }
+}
+
+Lesson04.init();
