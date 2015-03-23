be3ceebeccd868104423304d8da66ebb363cdc9a
Jose I <jiparis@gmail.com>
2015-03-23 21:03:15 +0100

    Servicios y promesas

---
+++ ng-samples/06/app.ts
@@ -0,0 +1,36 @@
+﻿/// <reference path="controllers/namesController.ts" />
+
+module Lesson06{
+    'use strict';
+
+    // nuestra aplicación angular. 
+    export var myApp: ng.IModule;
+
+    // clase de inicialización
+    class MyApp {
+        
+        /**
+        * Creación de la aplicación
+        */
+        static create() : ng.IModule {
+            return angular.module("myApp", [])
+                // controllers
+                .controller("namesController", NamesController) 
+                // filters
+                .filter("dash", DashFilter)
+                // value service
+                .value("superHeroes", superHeroes)
+                .value("maxElements", maxElements)
+                // factory service
+                .factory("randomHero", RandomHeroFactory)
+                // service service
+                .service("heroesService", SuperHeroesService);
+        }
+    };
+
+    export function init() {
+        myApp = MyApp.create();
+    }
+}
+
+Lesson06.init();
---
+++ ng-samples/07/app.ts
@@ -0,0 +1,23 @@
+﻿/// <reference path="../typings/angularjs/angular.d.ts" />
+/// <reference path="controllers/namesController.ts" />
+/// <reference path="filters/dashFilter.ts" />
+/// <reference path="services/namesServices.ts" />
+
+module Lesson06{
+    'use strict';
+
+    // nuestra aplicación angular.
+    var myApp: ng.IModule = angular.module("myApp", [])
+        // controllers
+        .controller("heroesController", HeroesController)
+        // filters
+        .filter("dash", DashFilter)
+        // value service
+        .value("superHeroes", superHeroes)
+        .value("maxElements", maxElements)
+        // factory service
+        .factory("randomHero", randomHeroFactory)
+        // service service
+        .service("heroesService", SuperHeroesService)
+        .provider("myServiceProvider", MyServiceProvider);
+}
