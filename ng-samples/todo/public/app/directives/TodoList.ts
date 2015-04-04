ef4b9a2c1d0f7e4c71f53aa503482c99afbb24fc
Jose I <jiparis@gmail.com>
2015-04-04 23:44:23 +0200

    Directiva TodoList

--- ng-samples/todo/public/app/directives/TodoList.ts
+++ ng-samples/todo/public/app/directives/TodoList.ts
@@ -1,3 +1,23 @@
-/**
- * Created by jose on 03/04/15.
- */
+/// <reference path="../../../../typings/angularjs/angular.d.ts" />
+/// <reference path="../services/TodoService.ts" />
+/// <reference path="../controllers/TodoController.ts" />
+
+
+module Todos{
+
+    export function TodoListDirective(): ng.IDirective{
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: "/app/directives/todoList.html",
+            controller: TodoController,
+            controllerAs: 'todolist',
+            scope: {
+                todos: '='
+            },
+            link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+
+            }
+        };
+    }
+}
